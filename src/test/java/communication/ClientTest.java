7588a7bd0e39b4ff3266a85c1559b9ef60968582
Tanvi <tanvi.pandit@hcl.com>
2021-03-24 23:00:39 -0400

    Init

---
+++ src/test/java/communication/ClientTest.java
@@ -0,0 +1,13 @@
+package communication;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.rules.TestRule;
+
+import java.io.IOException;
+
+public class ClientTest extends TestCase {
+
+
+
+}
